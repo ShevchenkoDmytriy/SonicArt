add212d84d1855004d97773d67067b15afed162e
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2022-12-07 19:42:12 +0200

    c#

---
+++ Animation1/Animation1/GameElement.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace Animation1
+{
+    public class GameElement
+    {
+        public TranslateTransform RenderTransform { get; internal set; }
+        public int PositionX { get; internal set; }
+    }
+}
